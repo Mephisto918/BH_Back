0e02634d5b11dddd627703377a1a82784aa77090
mephiscus <edward.camagong@wlcormoc.edu.ph>
2025-08-13 00:20:36 +0800

    small fix

--- frontend/src/features/landing/techstack.tsx
+++ frontend/src/features/landing/techstack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Box, Heading, Stack, Text } from '@chakra-ui/react';
+import { Heading, HStack, Stack, Text } from '@chakra-ui/react';
 import { techStackJson } from './techStachArray';
 import Icon from 'tech-stack-icons';
 import { Colors, ShadowDark, Spacing } from '../constants';
@@ -18,31 +18,38 @@ export default function TechStack() {
             fast as your ambitions.
           </Text>
         </Stack>
-        <Stack className="stack-row">
-          {techStackJson.map((tech, index) => (
-            <a key={index} className="item-container" href={tech.link}>
-              <Icon className="icon" name={tech.logo} />
-              <Text
-                mt={3}
-                fontWeight="semibold"
-                fontSize="lg"
-                color={Colors.TextInverse[1]}
+        <HStack className="stack-container">
+          <Stack className="stack-row">
+            {techStackJson.map((tech, index) => (
+              <a
+                key={index}
+                className="item-container"
+                href={tech.link}
+                // style={{ marginLeft: index === 0 ? 0 : "auto" }}
               >
-                {tech.name}
-              </Text>
-              {tech.description && (
+                <Icon className="icon" name={tech.logo} />
                 <Text
-                  mt={1}
-                  fontSize="sm"
-                  color={Colors.TextInverse[2]}
-                  lineHeight="tall"
+                  mt={3}
+                  fontWeight="semibold"
+                  fontSize="lg"
+                  color={Colors.TextInverse[1]}
                 >
-                  {tech.description}
+                  {tech.name}
                 </Text>
-              )}
-            </a>
-          ))}
-        </Stack>
+                {tech.description && (
+                  <Text
+                    mt={1}
+                    fontSize="sm"
+                    color={Colors.TextInverse[2]}
+                    lineHeight="tall"
+                  >
+                    {tech.description}
+                  </Text>
+                )}
+              </a>
+            ))}
+          </Stack>
+        </HStack>
       </Stack>
     </Section>
   );
@@ -67,46 +74,54 @@ const Section = styled.section`
       }
     }
 
-    > .stack-row {
+    > .stack-container {
       display: flex;
       flex-direction: row;
-      flex-wrap: nowrap;
-      max-width: 100%;
-      overflow-x: auto;
-      padding: 0 0 2rem 0;
-      /* overflow-y: hidden; */
-      gap: ${Spacing.lg};
+      justify-content: center;
 
-      ::-webkit-scrollbar {
-        display: none;
-      }
+      > .stack-row {
+        border: 1px solid green;
+        display: flex;
+        flex-direction: row;
+        overflow: auto;
+        flex-wrap: nowrap;
+        flex-shrink: 0;
 
-      > .item-container {
-        flex: 0 0 auto;
-        width: 180px;
-        border-radius: 12px;
-        padding: 1rem;
-        transition: box-shadow 0.3s ease;
-        background: ${Colors.PrimaryLight[6]};
+        max-width: 100%;
+        padding: 0 0 2rem 0;
+        gap: ${Spacing.lg};
 
-        box-shadow: ${ShadowDark.xxl};
-
-        &:hover {
-          background: ${Colors.PrimaryLight[5]};
-          box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        ::-webkit-scrollbar {
+          display: none;
         }
 
-        > .icon {
-          /* border: 2px solid yellow; */
-          aspect-ratio: 1;
-          height: 100px;
-          margin: 0 auto;
-          display: block;
-        }
+        > .item-container {
+          flex: 0 0 auto;
+          width: 180px;
+          border-radius: 12px;
+          padding: 1rem;
+          transition: box-shadow 0.3s ease;
+          background: ${Colors.PrimaryLight[6]};
 
-        > p,
-        > span {
-          margin-top: 0.5rem;
+          box-shadow: ${ShadowDark.xxl};
+
+          &:hover {
+            background: ${Colors.PrimaryLight[5]};
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+          }
+
+          > .icon {
+            /* border: 2px solid yellow; */
+            aspect-ratio: 1;
+            height: 100px;
+            margin: 0 auto;
+            display: block;
+          }
+
+          > p,
+          > span {
+            margin-top: 0.5rem;
+          }
         }
       }
     }
@@ -118,25 +133,44 @@ const Section = styled.section`
       font-size: 2.3rem;
       /* border: 2px solid green; */
     }
+    .stack-row {
+      justify-content: flex-start;
+    }
   }
-  
+
+  @media screen and (min-width: 768px) {
+    .heading {
+      font-size: 2.3rem;
+      /* border: 2px solid green; */
+    }
+    .stack-row {
+      justify-content: flex-start;
+    }
+  }
+
   /* Small desktop */
   @media screen and (min-width: 1024px) {
     .heading {
       font-size: 3rem;
       /* border: 2px solid green; */
     }
-    
-    .item {
-      max-width: 300px;
+    .stack-row {
+      /* justify-content: flex-start;
+      padding-left: 1rem;
+      padding-right: 1rem; */
     }
   }
-  
+
   /* Large desktop / Ultra-wide */
   @media screen and (min-width: 1440px) {
     .heading {
       font-size: 4rem;
       /* border: 2px solid yellow; */
+    }
+    .stack-row {
+      justify-content: space-between;
+      padding-left: 1rem;
+      padding-right: 1rem;
     }
   }
 `;

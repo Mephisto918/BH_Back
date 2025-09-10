94804e27ac26386fc4a9fa938ac17c07a5b65f31
mephiscus <edward.camagong@wlcormoc.edu.ph>
2025-09-10 11:56:59 +0800

    fix

--- frontend/src/features/admins/logs/screens/logs.main.screen.tsx
+++ frontend/src/features/admins/logs/screens/logs.main.screen.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 export default function LogsMainScreen() {
-  return <Container>Logs Yawa</Container>;
+  return <Container>Logs Testing</Container>;
 }
 
 const Container = styled.section``;
